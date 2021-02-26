4306dc702f5c7ca374eb404aef209b648380c181
Joao Almeida <jopecoal@gmail.com>
2021-02-26 15:36:17 +0000

    validator errors fixed

--- app/Validation/Validator.php
+++ app/Validation/Validator.php
@@ -9,18 +9,26 @@ class Validator{
     protected $errors = [];
 
     public function validate($request, array $rules){
+
+
+        // $validation->validate($request, [
+        //     'email' => v::noWhiteSpace()->notEmpty(),
+        //     'name' =>  v::noWhiteSpace()->notEmpty(),
+        //     'password' =>  v::noWhiteSpace()->notEmpty()
+        // ]);
+
         foreach ($rules as $field => $rule) {
             try{
                 $rule->setName($field)->assert($request->getParam($field));
             } catch (NestedValidationException $e){
-                $this->error[$field] = $e->getMessages();
+                $this->errors[$field] = $e->getMessages();
             }
         }
         return $this->errors;
     }
        
      public function failed(){
-            return !empty($this->erros);
+            return !empty($this->errors);
         }
       public function errors(){
           return $this->errors;
